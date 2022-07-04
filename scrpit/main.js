60078b416052f5c640811ab30a395d3830779460
Alex L <ff0nordver@gmail.com>
2022-07-04 14:00:36 +0300

    first screen - completed by 80%. working on the second screen

--- scrpit/main.js
+++ scrpit/main.js
@@ -3,3 +3,11 @@ function gettingAndsetHeight (elem, elem2) {
     document.querySelector(elem2).style.top = hElem + 10 + 'px';
     };
     gettingAndsetHeight('.bg-fashion', '.suggestions');
+
+function addimg (elem, elem2, elem3) {
+    let promoBlock = document.querySelectorAll(elem);
+    for (let i; i > elem; i++) {
+        elem.style.backgroundimg = elem2, elem3;
+    };
+}
+addimg('.benefits__promo-block', '../src/promo-block.png', '../src/promo-block2.png');
