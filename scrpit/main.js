100e20ab3844687aa10fa0aa2fe8d19493d2bd3d
Alex L <ff0nordver@gmail.com>
2022-07-08 15:58:02 +0300

    first screen and second screen - adaptive 0%

--- scrpit/main.js
+++ scrpit/main.js
@@ -1,6 +1,1 @@
-function gettingAndsetHeight (elem, elem2) {
-    let hElem = document.querySelector(elem).scrollHeight;
-    document.querySelector(elem2).style.top = hElem + 10 + 'px';
-    };
-    gettingAndsetHeight('.bg-fashion', '.suggestions');
 
